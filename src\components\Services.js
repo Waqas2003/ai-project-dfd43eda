12a2f578daaf744857916a610743e99e26fcf9c1
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:30:02 +0500

    Add src\components\Services.js

---
+++ src\components\Services.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Services.css';
+
+function Services() {
+  return (
+    <section className="services">
+      <h2>Services</h2>
+      <ul>
+        <li>Software Development</li>
+        <li>Consulting</li>
+        <li>Project Management</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Services;
