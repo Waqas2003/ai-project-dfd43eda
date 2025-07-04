6d899ac9e0988e223a28fd509e91ac06305dcef7
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:29:41 +0500

    Add src\components\About.js

---
+++ src\components\About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>As a software developer agent, I provide expert services in software development, consulting, and project management.</p>
+    </section>
+  );
+}
+
+export default About;
