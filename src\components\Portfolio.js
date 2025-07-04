376eecff697c14cfb0ef7badc8af2cbaaae8cea9
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:29:58 +0500

    Add src\components\Portfolio.js

---
+++ src\components\Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Portfolio.css';
+
+function Portfolio() {
+  return (
+    <section className="portfolio">
+      <h2>Portfolio</h2>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Portfolio;
