492b3d2ded547f31020da475ac4a975dc75d79d1
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:29:55 +0500

    Add src\components\Hero.js

---
+++ src\components\Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Software Developer Agent</h1>
+      <p>Expertise in software development and consulting services.</p>
+    </section>
+  );
+}
+
+export default Hero;
